75196249a2927d9da918960898d435da7435e223
MohsenAkhlaghi <mohi00720115@gmail.com>
2023-11-25 20:00:27 +0330

    Create remote Api

---
+++ app/src/main/java/com/example/foodapp/data/remote/IFoodRecipesApi.kt
@@ -0,0 +1,25 @@
+package com.example.foodapp.data.remote
+
+import com.example.foodapp.models.dto.FoodRecipe
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Query
+import retrofit2.http.QueryMap
+
+interface IFoodRecipesApi {
+
+    /**
+     * روش قدیمی کوئری نوشتن
+     */
+    /*@GET("/recipes/complexSearch")
+    suspend fun getRecipesOld(
+        @Query("number") number: Int = 10,
+        @Query("name") name: String = "Ali",
+    ): Response<FoodRecipe>*/
+
+    @GET("/recipes/complexSearch")
+    suspend fun getRecipes(
+        @QueryMap queries: Map<String, String>
+    ): Response<FoodRecipe>
+
+}
