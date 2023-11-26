c49f5e48a7473b92f757df96b30144dfe7bbb4ab
MohsenAkhlaghi <mohi00720115@gmail.com>
2023-11-26 12:39:18 +0330

    Edit Repository

--- app/src/main/java/com/example/foodapp/domain/repository/Repository.kt
+++ app/src/main/java/com/example/foodapp/domain/repository/Repository.kt
@@ -4,7 +4,9 @@ import com.example.foodapp.data.datasource.RemoteDataSource
 import dagger.hilt.android.scopes.ActivityRetainedScoped
 import javax.inject.Inject
 
+//این انوتیشن میاد ریپازیتوری رو وصل میکنه به چرخه حیات اکتیویتی و در نتیجه کانفیگوریشن چنج (چرخش صفجه) رو میتونه هندل کنه
 @ActivityRetainedScoped
 class Repository @Inject constructor(private val remoteDataSource: RemoteDataSource) {
-    val remote = remoteDataSource
+//    val remote = remoteDataSource
+
 }
