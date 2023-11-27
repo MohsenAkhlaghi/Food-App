cc2472c5042377f0672c7d4b3326dae496077a13
MohsenAkhlaghi <mohi00720115@gmail.com>
2023-11-27 18:20:30 +0330

    Add counts

--- app/src/main/java/com/example/foodapp/util/Constants.kt
+++ app/src/main/java/com/example/foodapp/util/Constants.kt
@@ -3,4 +3,11 @@ package com.example.foodapp.util
 object Constants {
     const val API_KEY = "68fc87f4ade748d2beb7f6543533a212"
     const val BASE_URL = "https://api.spoonacular.com"
+    //API Query Keys
+    const val QUERY_NUMBER = "number"
+    const val QUERY_API_KEY = "apiKey"
+    const val QUERY_TYPE = "type"
+    const val QUERY_DIET = "diet"
+    const val QUERY_ADD_RECIPE_INFORMATION = "addRecipeInformation"
+    const val QUERY_FILL_INGREDIENTS = "fillIngredients"
 }
