a2fdb132e7db19ec8ed42232681ca910132060d1
MohsenAkhlaghi <mohi00720115@gmail.com>
2023-11-27 20:33:03 +0330

    Add Local DataSource in Repository

--- app/src/main/java/com/example/foodapp/domain/repository/Repository.kt
+++ app/src/main/java/com/example/foodapp/domain/repository/Repository.kt
@@ -1,12 +1,13 @@
 package com.example.foodapp.domain.repository
 
+import com.example.foodapp.data.datasource.LocalDataSource
 import com.example.foodapp.data.datasource.RemoteDataSource
 import dagger.hilt.android.scopes.ActivityRetainedScoped
 import javax.inject.Inject
 
 //این انوتیشن میاد ریپازیتوری رو وصل میکنه به چرخه حیات اکتیویتی و در نتیجه کانفیگوریشن چنج (چرخش صفجه) رو میتونه هندل کنه
 @ActivityRetainedScoped
-class Repository @Inject constructor(val remoteDataSource: RemoteDataSource) {
+class Repository @Inject constructor(val remoteDataSource: RemoteDataSource, val localDataSource: LocalDataSource) {
 //    val remote = remoteDataSource
 
 }
