7544f335c2cdcd24ea6c8712413c8e6216cc9bf6
MohsenAkhlaghi <mohi00720115@gmail.com>
2023-11-28 14:42:13 +0330

    Edit package Name

---
+++ app/src/main/java/com/example/foodapp/ui/recipes/RecipesViewModel.kt
@@ -0,0 +1,61 @@
+package com.example.foodapp.ui.recipes
+
+import android.app.Application
+import androidx.datastore.dataStore
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.foodapp.data.DataStoreRepository
+import com.example.foodapp.util.Constants.API_KEY
+import com.example.foodapp.util.Constants.DEFAULT_DIET_TYPE
+import com.example.foodapp.util.Constants.DEFAULT_MEAL_TYPE
+import com.example.foodapp.util.Constants.QUERY_ADD_RECIPE_INFORMATION
+import com.example.foodapp.util.Constants.QUERY_API_KEY
+import com.example.foodapp.util.Constants.QUERY_DIET
+import com.example.foodapp.util.Constants.QUERY_FILL_INGREDIENTS
+import com.example.foodapp.util.Constants.QUERY_NUMBER
+import com.example.foodapp.util.Constants.QUERY_TYPE
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class RecipesViewModel @Inject constructor(
+    application: Application,
+    private val dataStoreRepository: DataStoreRepository
+) : AndroidViewModel(application) {
+
+    var mealType = DEFAULT_MEAL_TYPE
+    var dietType = DEFAULT_DIET_TYPE
+
+    val readMealAndDietType = dataStoreRepository.readMealAndDietType
+
+    fun saveMealAndDietType(mealType: String, mealTypeId: Int, dietType: String, dietTypeId: Int): Job {
+        return viewModelScope.launch {
+            dataStoreRepository.saveMealAndDietType(mealType, mealTypeId, dietType, dietTypeId)
+        }
+    }
+
+
+    fun applyQueries(): HashMap<String, String> {
+        val queries: HashMap<String, String> = HashMap()
+
+        viewModelScope.launch {
+            readMealAndDietType.collect{value ->
+                mealType = value.selectedMealType
+                dietType = value.selectedDietType
+            }
+        }
+
+        //تعداد کوئری و نتایجی که می خوایم برگردونیم
+        queries[QUERY_NUMBER] = "50"
+        queries[QUERY_API_KEY] = API_KEY
+        queries[QUERY_TYPE] = "snack"
+        queries[QUERY_DIET] = "vegan"
+        queries[QUERY_ADD_RECIPE_INFORMATION] = "true"
+        queries[QUERY_FILL_INGREDIENTS] = "true"
+        return queries
+    }
+
+}
