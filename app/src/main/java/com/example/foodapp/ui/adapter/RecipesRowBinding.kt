57ea86b09b41daeadde5ac90a1d13eb059f76b25
MohsenAkhlaghi <mohi00720115@gmail.com>
2023-11-30 18:09:48 +0330

    Add bindingAdapter for onRecipesClickListener

--- app/src/main/java/com/example/foodapp/ui/adapter/RecipesRowBinding.kt
+++ app/src/main/java/com/example/foodapp/ui/adapter/RecipesRowBinding.kt
@@ -1,16 +1,36 @@
 package com.example.foodapp.ui.adapter
 
+import android.content.ContentValues.TAG
+import android.util.Log
 import android.view.View
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.content.ContextCompat
 import androidx.databinding.BindingAdapter
+import androidx.navigation.findNavController
 import coil.load
 import com.example.foodapp.R
+import com.example.foodapp.models.dto.Result
+import com.example.foodapp.ui.recipes.RecipesFragmentDirections
 
 class RecipesRowBinding {
 
     companion object {
+
+        @BindingAdapter("onRecipesClickListener")
+        @JvmStatic
+        fun onRecipesClickListener(recipesRowLayout: ConstraintLayout, result: Result) {
+            Log.e("onRecipesClickListener", "CALLED")
+            recipesRowLayout.setOnClickListener {
+                try {
+                    val action = RecipesFragmentDirections.actionRecipesFragmentToDetailsActivity(result)
+                    recipesRowLayout.findNavController().navigate(action)
+                } catch (e: Exception) {
+                    Log.e(TAG, "onRecipesClickListener: ${e.message}")
+                }
+            }
+        }
 
         @BindingAdapter("loadImageUrl")
         @JvmStatic
