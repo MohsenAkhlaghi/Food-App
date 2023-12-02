1a096f23d6fb07811ed377731bd5592d26c90821
MohsenAkhlaghi <mohi00720115@gmail.com>
2023-12-02 21:50:20 +0330

    Add FavoriteEntity in Database

--- app/src/main/java/com/example/foodapp/data/local/RecipesDatabase.kt
+++ app/src/main/java/com/example/foodapp/data/local/RecipesDatabase.kt
@@ -4,7 +4,7 @@ import androidx.room.Database
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
 
-@Database(entities = [RecipesEntity::class], version = 1, exportSchema = false)
+@Database(entities = [RecipesEntity::class, FavoriteEntity::class], version = 1, exportSchema = false)
 @TypeConverters(RecipesTypeConverter::class)
 abstract class RecipesDatabase : RoomDatabase() {
     abstract fun recipesDao(): RecipesDao
