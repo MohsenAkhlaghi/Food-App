08c9129b47f0626079015aeac27588d7415b234b
MohsenAkhlaghi <mohi00720115@gmail.com>
2023-12-04 13:24:17 +0330

    Recipe Selection Style onLongClick

--- app/src/main/java/com/example/foodapp/ui/adapter/FavoriteRecipesAdapter.kt
+++ app/src/main/java/com/example/foodapp/ui/adapter/FavoriteRecipesAdapter.kt
@@ -15,12 +15,16 @@ import com.example.foodapp.R
 import com.example.foodapp.data.local.FavoriteEntity
 import com.example.foodapp.databinding.FavoriteRecipesRowLayoutBinding
 import com.example.foodapp.ui.favorite.FavoriteRecipesFragmentDirections
+import com.google.android.material.card.MaterialCardView
 
 class FavoriteRecipesAdapter(
     private val requireActivity: FragmentActivity
 ) : RecyclerView.Adapter<FavoriteRecipesAdapter.MyViewHolder>(), ActionMode.Callback {
 
     private var favoriteRecipes = emptyList<FavoriteEntity>()
+    private var selectedRecipes = arrayListOf<FavoriteEntity>()
+    private var myViewHolders = arrayListOf<MyViewHolder>()
+    private var multiSelection = false
 
     class MyViewHolder(private val binding: FavoriteRecipesRowLayoutBinding) :
         RecyclerView.ViewHolder(binding.root) {
@@ -45,24 +49,55 @@ class FavoriteRecipesAdapter(
     }
 
     override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
-        val selectedRecipe = favoriteRecipes[position]
-        holder.bind(selectedRecipe)
+        myViewHolders.add(holder)
+
+        val currentRecipe = favoriteRecipes[position]
+        holder.bind(currentRecipe)
 
         /**
          * Single Click Listener
          */
         holder.itemView.findViewById<ConstraintLayout>(R.id.favorite_recipes_row_layout).setOnClickListener {
-            val action = FavoriteRecipesFragmentDirections.actionFavoriteRecipesFragmentToDetailsActivity(selectedRecipe.result)
-            holder.itemView.findNavController().navigate(action)
+            if (multiSelection) {
+                applySelection(holder, currentRecipe)
+            } else {
+                val action = FavoriteRecipesFragmentDirections.actionFavoriteRecipesFragmentToDetailsActivity(currentRecipe.result)
+                holder.itemView.findNavController().navigate(action)
+            }
         }
 
         /**
          * Long Click Listener
          */
         holder.itemView.findViewById<ConstraintLayout>(R.id.favorite_recipes_row_layout).setOnLongClickListener {
-            requireActivity.startActionMode(this)
-            true
+            if (!multiSelection) {
+                multiSelection = true
+                requireActivity.startActionMode(this)
+                applySelection(holder, currentRecipe)
+                true
+            } else {
+                multiSelection = true
+                false
+            }
         }
+    }
+
+    private fun applySelection(holder: MyViewHolder, currentRecipes: FavoriteEntity) {
+        if (selectedRecipes.contains(currentRecipes)) {
+            selectedRecipes.remove(currentRecipes)
+            changeRecipesStyle(holder, R.color.cardBackgroundColor, R.color.strokeColor)
+        } else {
+            selectedRecipes.add(currentRecipes)
+            changeRecipesStyle(holder, R.color.card_background_light_color, R.color.color_primary)
+        }
+    }
+
+    private fun changeRecipesStyle(holder: MyViewHolder, backgroundColor: Int, strokeColor: Int) {
+        holder.itemView.findViewById<ConstraintLayout>(R.id.favorite_recipes_row_layout).setBackgroundColor(
+            ContextCompat.getColor(requireActivity, backgroundColor)
+        )
+        holder.itemView.findViewById<MaterialCardView>(R.id.favorite_row_cardView).strokeColor =
+            ContextCompat.getColor(requireActivity, strokeColor)
     }
 
     override fun getItemCount(): Int {
@@ -92,6 +127,11 @@ class FavoriteRecipesAdapter(
     }
 
     override fun onDestroyActionMode(actionMode: ActionMode?) {
+        myViewHolders.forEach { holder ->
+            changeRecipesStyle(holder, R.color.cardBackgroundColor, R.color.strokeColor)
+        }
+        multiSelection = false
+        selectedRecipes.clear()
         applyStatusBarColor(R.color.status_bar_color)
     }
 
