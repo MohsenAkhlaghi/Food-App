8d1c8f96861113b94c9a7bbbdc1f91ddaff95a93
MohsenAkhlaghi <mohi00720115@gmail.com>
2023-12-05 14:12:14 +0330

    Use menu & intent for share

--- app/src/main/java/com/example/foodapp/ui/foodjoke/FoodJokeFragment.kt
+++ app/src/main/java/com/example/foodapp/ui/foodjoke/FoodJokeFragment.kt
@@ -1,8 +1,12 @@
 package com.example.foodapp.ui.foodjoke
 
 import android.content.ContentValues.TAG
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.view.View
 import android.widget.Toast
 import androidx.databinding.DataBindingUtil
@@ -21,29 +25,53 @@ import kotlinx.coroutines.launch
 class FoodJokeFragment : Fragment(R.layout.fragment_food_joke) {
     private lateinit var binding: FragmentFoodJokeBinding
     private val viewModelMain: MainViewModel by viewModels()
+    private var foodJoke = "No Food Joke"
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding = DataBindingUtil.bind(view)!!
-        binding.lifecycleOwner = viewLifecycleOwner
-        binding.viewModelMain = viewModelMain
-        viewModelMain.getFoodJokes(API_KEY)
-        viewModelMain.foodJokeResponse.observe(viewLifecycleOwner) { response ->
-            when (response) {
-                is NetworkResult.Success -> {
-                    binding.foodJokeTextView.text = response.data?.text
-                }
+        with(binding) {
+            lifecycleOwner = viewLifecycleOwner
+            viewModelMain = viewModelMain
+            setHasOptionsMenu(true)
+            viewModelMain.getFoodJokes(API_KEY)
+            viewModelMain.foodJokeResponse.observe(viewLifecycleOwner) { response ->
+                when (response) {
+                    is NetworkResult.Success -> {
+                        foodJokeTextView.text = response.data?.text
+                        if (response.data != null) {
+                            foodJoke = response.data.text
+                        }
+                    }
 
-                is NetworkResult.Error -> {
-                    loadDataFromCache()
-                    Toast.makeText(requireContext(), response.message.toString(), Toast.LENGTH_SHORT).show()
-                }
+                    is NetworkResult.Error -> {
+                        loadDataFromCache()
+                        Toast.makeText(requireContext(), response.message.toString(), Toast.LENGTH_SHORT).show()
+                    }
 
-                is NetworkResult.Loading -> {
-                    Log.e(TAG, "FragmentFoodJoke: LOADING")
+                    is NetworkResult.Loading -> {
+                        Log.e(TAG, "FragmentFoodJoke: LOADING")
+                    }
                 }
             }
         }
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.food_joke_menu, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == R.id.share_food_joke_menu) {
+            val shareIntent = Intent().apply {
+                this.action = Intent.ACTION_SEND
+                this.putExtra(Intent.EXTRA_TEXT, foodJoke)
+                this.type = "text/plain"
+            }
+            startActivity(shareIntent)
+        }
+        return super.onOptionsItemSelected(item)
     }
 
     private fun loadDataFromCache() {
@@ -51,6 +79,7 @@ class FoodJokeFragment : Fragment(R.layout.fragment_food_joke) {
             viewModelMain.readFoodJoke.observe(viewLifecycleOwner) { database ->
                 if (!database.isNullOrEmpty()) {
                     binding.foodJokeTextView.text = database.first().foodJoke.text
+                    foodJoke = database.first().foodJoke.text
                 }
             }
         }
