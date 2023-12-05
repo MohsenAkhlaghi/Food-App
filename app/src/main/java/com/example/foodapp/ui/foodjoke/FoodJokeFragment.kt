23e0c290b0706a607f580f85c3fb17e6a690ac30
MohsenAkhlaghi <mohi00720115@gmail.com>
2023-12-05 12:28:26 +0330

    Create & Start FoodJokeFragment.kt

--- app/src/main/java/com/example/foodapp/ui/foodjoke/FoodJokeFragment.kt
+++ app/src/main/java/com/example/foodapp/ui/foodjoke/FoodJokeFragment.kt
@@ -1,21 +1,59 @@
 package com.example.foodapp.ui.foodjoke
 
+import android.content.ContentValues.TAG
 import android.os.Bundle
+import android.util.Log
 import android.view.View
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
 import com.example.foodapp.R
 import com.example.foodapp.databinding.FragmentFoodJokeBinding
-import com.example.foodapp.databinding.FragmentRecipesBinding
+import com.example.foodapp.ui.MainViewModel
+import com.example.foodapp.util.Constants.API_KEY
+import com.example.foodapp.util.NetworkResult
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
 
+@AndroidEntryPoint
 class FoodJokeFragment : Fragment(R.layout.fragment_food_joke) {
     private lateinit var binding: FragmentFoodJokeBinding
+    private val viewModelMain: MainViewModel by viewModels()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding = DataBindingUtil.bind(view)!!
         binding.lifecycleOwner = viewLifecycleOwner
+        binding.viewModelMain = viewModelMain
+        viewModelMain.getFoodJokes(API_KEY)
+        viewModelMain.foodJokeResponse.observe(viewLifecycleOwner) { response ->
+            when (response) {
+                is NetworkResult.Success -> {
+                    binding.foodJokeTextView.text = response.data?.text
+                }
+
+                is NetworkResult.Error -> {
+                    loadDataFromCache()
+                    Toast.makeText(requireContext(), response.message.toString(), Toast.LENGTH_SHORT).show()
+                }
+
+                is NetworkResult.Loading -> {
+                    Log.e(TAG, "FragmentFoodJoke: LOADING")
+                }
+            }
+        }
     }
 
+    fun loadDataFromCache() {
+        lifecycleScope.launch {
+            viewModelMain.readFoodJoke.observe(viewLifecycleOwner) { database ->
+                if (database.isNotEmpty() && database != null) {
+                    binding.foodJokeTextView.text = database[0].text
+                }
+            }
+        }
+    }
 
 }
